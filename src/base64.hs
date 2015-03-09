29fa5b0d18b4e9599f99ba9ffeba2f2ba4475011
John Downs <jad719@pubbox38.cims.nyu.edu>
2015-03-09 19:00:23 -0400

    first commit

---
+++ src/base64.hs
@@ -0,0 +1,15 @@
+-- Base64 decode and encode bytes
+
+module Base64 where
+ 
+import qualified Data.ByteString as B
+
+-- pull off 8 bytes - if this fails, try until none left, and pad with =
+-- encode bytes, encode tail
+
+encode8 :: B -> string
+encode8 s :: i
+encode None :: ""
+encode8 val = ???
+
+
